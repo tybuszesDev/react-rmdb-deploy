9fc984f93f99d01bb424fd752d370f5d8b5febcf
Łukasz Tybuszewski <tybuszes@protonmail.com>
2020-04-23 04:09:41 +0200

    initial heroku commit 2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./layouts/App";
 import "./styles/index.css";
 
+App.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    App.settings.env
+  );
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
